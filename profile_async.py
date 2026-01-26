4c684a073a30c278eec59bc4313179d7d2680204
tiberiuiancu <tiberiu.iancu@gmail.com>
2026-01-26 15:50:12 +0100

    merge sparse scaling into profile

--- profile_async.py
+++ profile_async.py
@@ -1,15 +1,55 @@
+import time
+import os
+
+from pcgcn.data import load_and_process_dataset
+from pcgcn.models import PCGCN
+
 import torch
-from pcgcn.layers import GraphConvPartLayer
-from sparse_scaling import _process, gen_graph
 from torch.profiler import ProfilerActivity, profile
+import dgl
+import networkx as nx
 
 
-def profile_fwd(eb, splits, x: torch.Tensor, hidden_size: int = 1024, preload: int = 0):
-    layer = GraphConvPartLayer(
-        x.shape[1], hidden_size, eb, splits, device="cuda", preload_adj=preload
+os.makedirs("traces", exist_ok=True)
+
+
+def gen_graph(n: int, m: int = 100):
+    return nx.barabasi_albert_graph(n, m, seed=0)
+
+
+def _process(g, k, autotune_hidden_size: int = None, preload: int = 0):
+    (eb, _, _, _, _, _) = load_and_process_dataset(
+        dgl.from_networkx(g),
+        k,
+        autotune_hidden_size=autotune_hidden_size,
+        preload_adj=preload,
+        device="cuda",
     )
 
-    _handler = lambda prof: prof.export_chrome_trace(f"trace_{preload}.json")
+    return eb
+
+
+def profile_fwd(
+    k: int,
+    x: torch.Tensor,
+    hidden_size: int = 1024,
+    out_size: int = 128,
+    preload: int = 0,
+):
+    n = x.shape[0]
+    eb = _process(g, k, autotune_hidden_size=hidden_size, preload=preload)
+    model = PCGCN(
+        in_size=x.shape[1],
+        out_size=out_size,
+        hidden_size=hidden_size,
+        n_layers=3,
+        eb=eb,
+        device="cuda",
+    )
+
+    _handler = lambda prof: prof.export_chrome_trace(
+        f"traces/trace_{n}_{k}_{preload}.json"
+    )
     with profile(
         activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA],
         on_trace_ready=_handler,
@@ -17,23 +57,23 @@ def profile_fwd(eb, splits, x: torch.Tensor, hidden_size: int = 1024, preload: i
         profile_memory=True,
         with_stack=True,
     ) as prof:
-        layer(x)
+        start = time.time()
+        model(x)
+        return time.time() - start
 
 
 if __name__ == "__main__":
     n = 2**14
+    m = 100
     k = 2
     feat_size = 512
     hidden_size = 1024
-    autotune_hidden_size = hidden_size
+    out_size = 128
 
-    g = gen_graph(n)
+    g = gen_graph(n, m)
     print(f"Nodes: {g.number_of_nodes()}, edges: {g.number_of_edges()}")
     x = torch.rand((n, feat_size), device="cuda", dtype=torch.float32)
-    eb, splits = _process(
-        g, k, autotune_hidden_size=autotune_hidden_size, preload=False
-    )
 
-    profile_fwd(eb, splits, x, hidden_size=hidden_size, preload=0)
-    profile_fwd(eb, splits, x, hidden_size=hidden_size, preload=1)
-    profile_fwd(eb, splits, x, hidden_size=hidden_size, preload=2)
+    profile_fwd(k, x, hidden_size=hidden_size, preload=0)
+    profile_fwd(k, x, hidden_size=hidden_size, preload=1)
+    profile_fwd(k, x, hidden_size=hidden_size, preload=2)
