463985b94cec1a8ddddd2a810105a052081e07bf
tiberiuiancu <tiberiu.iancu@gmail.com>
2026-01-30 14:32:59 +0100

    run

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-# mvals=(10 100 1000)
-mvals=(100 1000)
-kvals=(5 4 3 2 1)
-nvals=($(for i in {11..18}; do echo $((2**i)); done))
+# mvals=(10 100 200)
+mvals=(200)
+# kvals=(5 4 3 2 1)
+kvals=(3 2 1)
+# nvals=($(for i in {11..18}; do echo $((2**i)); done))
+nvals=($(for i in {11..16}; do echo $((2**i)); done))
 
 for m in "${mvals[@]}"; do
     for k in "${kvals[@]}"; do
@@ -17,12 +19,12 @@ for m in "${mvals[@]}"; do
     done
 done
 
-# for m in "${mvals[@]}"; do
-#     for n in "${nvals[@]}"; do
-#         python profile_async.py --n "$n" --m "$m" --k "1" --force_sparse
-#         exit_status=$?
-#         if [ $exit_status -ne 0 ]; then
-#             break
-#         fi
-#     done
-# done
+for m in "${mvals[@]}"; do
+    for n in "${nvals[@]}"; do
+        python profile_async.py --n "$n" --m "$m" --k "1" --force_sparse
+        exit_status=$?
+        if [ $exit_status -ne 0 ]; then
+            break
+        fi
+    done
+done
