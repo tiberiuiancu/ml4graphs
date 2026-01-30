3c82ac4ebb3139d6cf88b40d70e513fc19e8b3d2
tiberiuiancu <tiberiu.iancu@gmail.com>
2026-01-30 17:09:45 +0100

    proper run

--- run.sh
+++ run.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-# mvals=(10 100 200)
-mvals=(200)
-# kvals=(5 4 3 2 1)
-kvals=(3 2 1)
-# nvals=($(for i in {11..18}; do echo $((2**i)); done))
-nvals=($(for i in {11..16}; do echo $((2**i)); done))
+mvals=(10 100 200)
+kvals=(5 4 3 2 1)
+nvals=($(for i in {11..18}; do echo $((2**i)); done))
 
 for m in "${mvals[@]}"; do
     for k in "${kvals[@]}"; do
